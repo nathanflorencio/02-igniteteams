8fe989c8fbdb892dffc728d569519275b0f043b4
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-10-04 16:48:23 -0300

    Finished Header Component.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
+
+import theme from './src/theme'
+
 import { Groups } from '@screens/Groups'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <>
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
